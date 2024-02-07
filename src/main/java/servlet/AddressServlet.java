469bbd9cf197e1cdedae2f4f3a88dd7ccba8828c
carlosb <carlosbgrafico@gmail.com>
2024-02-07 17:35:58 +0100

    .

---
+++ src/main/java/servlet/AddressServlet.java
@@ -0,0 +1,69 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entities.Address;
+import entitiesDAO.AddressDAO;
+import jakarta.persistence.EntityManager;
+import tools.JpaUtil;
+
+/**
+ * Servlet implementation class AddressServlet
+ */
+public class AddressServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private EntityManager em = JpaUtil.getEM("HibernateOracle");
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AddressServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Address variables.
+		long id = Long.parseLong(request.getParameter("idDireccion"));
+
+		String name = request.getParameter("nombreDireccion");
+		String province = request.getParameter("provinciaDireccion");
+		String city = request.getParameter("ciudadDireccion");
+		String postalCode = request.getParameter("codigoPostalDireccion");
+
+		Address address = new Address(name, province, city, postalCode);
+
+		AddressDAO addressDAO = new AddressDAO(em);
+
+		if (id == 0) {
+			addressDAO.insertAddress(address);
+		} else if (id < 0) {
+			address.setId(id);
+			addressDAO.updateAddress(id, address);
+		}
+		
+		doGet(request, response);
+	}
+
+}
